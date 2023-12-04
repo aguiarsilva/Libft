ae43f7e70d59e7413d0d4622fe6f8819cc7f4658
Bruno Aguiar <aguiarsilva@gmail.com>
2023-12-04 14:15:46 +0100

    updated files

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: baguiar- <baguiar-@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 13:42:31 by baguiar-          #+#    #+#             */
-/*   Updated: 2023/12/04 13:44:45 by baguiar-         ###   ########.fr       */
+/*   Updated: 2023/12/04 14:15:41 by baguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strchr(const char *s, int c)
     ptr = (char *)s;
 	  while (*ptr != c)
 	  {
-	  	if (*ptr == '/0')
+	  	if (*ptr == '\0')
 		  	return (NULL);
 		  ptr++;
 	  }
