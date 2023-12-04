e14aae643a0ea4079ff61e38e7cb58ab7f6ca80f
Bruno Aguiar <aguiarsilva@gmail.com>
2023-12-04 13:31:56 +0100

    updated files

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: baguiar- <baguiar-@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 13:33:36 by baguiar-          #+#    #+#             */
-/*   Updated: 2023/12/02 14:07:58 by baguiar-         ###   ########.fr       */
+/*   Updated: 2023/12/04 13:31:45 by baguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 {
-	size_t  i;
+	  size_t  i;
+    size_t  lensrc;
 
-	if (!n)
-		return (0);
-	else if (n == 0)
-		return (ft_strlen(src));
-	i = 0;
-	if (src[i] != '\0' && (i < (n - 1)))
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = 0;
-	return (ft_strlen(src));
+    lensrc = ft_strlen(src);
+    i = 0;
+	  if (n != 0)
+    {
+	    while (src[i] != '\0' && (i < (n - 1)))
+	    {
+		    dest[i] = src[i];
+		    i++;
+	    }
+	    dest[i] = '\0';
+    }
+	  return (lensrc);
 }
