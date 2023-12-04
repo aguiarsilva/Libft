a99756a5cf874dcfaeb4b035f85979e6100213c2
Bruno Aguiar <aguiarsilva@gmail.com>
2023-12-04 14:35:28 +0100

    updated files

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: baguiar- <baguiar-@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 13:42:31 by baguiar-          #+#    #+#             */
-/*   Updated: 2023/12/04 14:21:56 by baguiar-         ###   ########.fr       */
+/*   Updated: 2023/12/04 14:35:15 by baguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,11 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-        char  *ptr;
-
-        ptr = (char *)s;
-	      while (*ptr != '\0')
+	      while (*s != '\0')
 	      {
-	  	    if (*ptr == c)
-		  	    return ((char *)ptr);
-		      ptr++;
+	  	    if (*s == c)
+		  	    return ((char *)s);
+		      s++;
 	      }
 	      return (0);
 }
