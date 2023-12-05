d4bde3eb344d88d1da9c2268e24985432ceecc8d
Bruno Aguiar <aguiarsilva@gmail.com>
2023-12-05 13:10:20 +0100

    updated lstmapi

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: baguiar- <baguiar-@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 17:32:02 by baguiar-          #+#    #+#             */
-/*   Updated: 2023/12/04 17:56:45 by baguiar-         ###   ########.fr       */
+/*   Updated: 2023/12/05 13:10:05 by baguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
               ft_lstclear(&nlist, del);
               return (0);
           }
-          ft_lstaddback(&nlist, nnode);
+          ft_lstadd_back(&nlist, nnode);
           lst = lst->next;
       }
       return (nlist);
